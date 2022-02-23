6bc6734e7a416550df45faf9dff9ca463896097a
fachridantm <fachridantm@gmail.com>
2022-02-24 00:36:15 +0700

    ✨ Add ProfileActivity.kt

---
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.fundamental.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
