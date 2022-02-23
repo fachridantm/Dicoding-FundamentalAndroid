466049ad86b48cb6870731ac936b994901c1a0e8
fachridantm <fachridantm@gmail.com>
2022-02-23 14:50:11 +0700

    Add HomeFragment

--- MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/MainActivity.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dicoding.fundamental.myflexiblefragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
