14e18249be078e192cbee73e2ded7bf24f4f1ff2
fachridantm <fachridantm@gmail.com>
2022-02-24 00:35:28 +0700

    ✨ Update OptionDialogFragment.kt and DetailCategoryFragment.kt

--- MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/DetailCategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.fundamental.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class DetailCategoryFragment : Fragment() {
 
@@ -14,8 +16,13 @@ class DetailCategoryFragment : Fragment() {
     lateinit var tvCategoryDescription: TextView
     lateinit var btnProfile: Button
     lateinit var btnShowDialog: Button
-
     var description: String? = null
+    internal var optionDialogListener: OptionDialogFragment.OnOptionDialogListener =
+        object : OptionDialogFragment.OnOptionDialogListener {
+            override fun onOptionChosen(text: String?) {
+                Toast.makeText(requireActivity(), text, Toast.LENGTH_SHORT).show()
+            }
+        }
 
     companion object {
         var EXTRA_NAME = "extra_name"
@@ -32,22 +39,35 @@ class DetailCategoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         tvCategoryName = view.findViewById(R.id.tv_category_name)
         tvCategoryDescription = view.findViewById(R.id.tv_category_description)
         btnProfile = view.findViewById(R.id.btn_profile)
         btnShowDialog = view.findViewById(R.id.btn_show_dialog)
 
-        if (savedInstanceState != null) {
-            val descFromBundle = savedInstanceState.getString(EXTRA_DESCRIPTION)
-            description = descFromBundle
+        btnShowDialog.setOnClickListener {
+            val mOptionDialogFragment = OptionDialogFragment()
+
+            val mFragmentManager = childFragmentManager
+            mOptionDialogFragment.show(
+                mFragmentManager,
+                OptionDialogFragment::class.java.simpleName
+            )
+
+            if (savedInstanceState != null) {
+                val descFromBundle = savedInstanceState.getString(EXTRA_DESCRIPTION)
+                description = descFromBundle
+            }
+
+            if (arguments != null) {
+                val categoryName = arguments?.getString(EXTRA_NAME)
+                tvCategoryName.text = categoryName
+                tvCategoryDescription.text = description
+            }
         }
 
-        if (arguments != null) {
-            val categoryName = arguments?.getString(EXTRA_NAME)
-
-            tvCategoryName.text = categoryName
-            tvCategoryDescription.text = description
+        btnProfile.setOnClickListener {
+            val mIntent = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(mIntent)
         }
     }
 }
--- MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/OptionDialogFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/fundamental/myflexiblefragment/OptionDialogFragment.kt
@@ -1,59 +1,77 @@
 package com.dicoding.fundamental.myflexiblefragment
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.DialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class OptionDialogFragment : DialogFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OptionDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OptionDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var btnChoose: Button
+    private lateinit var btnClose: Button
+    private lateinit var rgOptions: RadioGroup
+    private lateinit var rbSaf: RadioButton
+    private lateinit var rbMou: RadioButton
+    private lateinit var rbLvg: RadioButton
+    private lateinit var rbMoyes: RadioButton
+    private var optionDialogListener: OnOptionDialogListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_option_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OptionDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OptionDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnChoose = view.findViewById(R.id.btn_choose)
+        btnClose = view.findViewById(R.id.btn_close)
+        rgOptions = view.findViewById(R.id.rg_options)
+        rbSaf = view.findViewById(R.id.rb_saf)
+        rbLvg = view.findViewById(R.id.rb_lvg)
+        rbMou = view.findViewById(R.id.rb_mou)
+        rbMoyes = view.findViewById(R.id.rb_moyes)
+        btnChoose.setOnClickListener {
+            val checkedRadioButtonId = rgOptions.checkedRadioButtonId
+            if (checkedRadioButtonId != -1) {
+                val coach: String? = when (checkedRadioButtonId) {
+                    R.id.rb_saf -> rbSaf.text.toString().trim()
+                    R.id.rb_mou -> rbMou.text.toString().trim()
+                    R.id.rb_lvg -> rbLvg.text.toString().trim()
+                    R.id.rb_moyes -> rbMoyes.text.toString().trim()
+                    else -> null
                 }
+                optionDialogListener?.onOptionChosen(coach)
+                dialog?.dismiss()
             }
+        }
+        btnClose.setOnClickListener {
+            dialog?.cancel()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val fragment = parentFragment
+
+        if (fragment is DetailCategoryFragment) {
+            this.optionDialogListener = fragment.optionDialogListener
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        this.optionDialogListener = null
+    }
+
+    interface OnOptionDialogListener {
+        fun onOptionChosen(text: String?)
     }
 }
