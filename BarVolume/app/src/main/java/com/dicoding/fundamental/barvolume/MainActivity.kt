16f7c2b196c8db5f589170570891c2e54483ee78
fachridantm <fachridantm@gmail.com>
2022-02-28 23:52:34 +0700

    ✨ Update BarVolume with ViewBinding

--- BarVolume/app/src/main/java/com/dicoding/fundamental/barvolume/MainActivity.kt
+++ BarVolume/app/src/main/java/com/dicoding/fundamental/barvolume/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.dicoding.fundamental.barvolume
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.fundamental.barvolume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+//    private lateinit var edtWidth: EditText
+//    private lateinit var edtHeight: EditText
+//    private lateinit var edtLength: EditText
+//    private lateinit var btnCalculate: Button
+//    private lateinit var tvResult: TextView
+
+    private lateinit var binding: ActivityMainBinding
 
     companion object {
         private const val STATE_RESULT = "state_result"
@@ -21,52 +21,56 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnCalculate.setOnClickListener(this)
+//        edtWidth = findViewById(R.id.edt_width)
+//        edtHeight = findViewById(R.id.edt_height)
+//        edtLength = findViewById(R.id.edt_length)
+//        btnCalculate = findViewById(R.id.btn_calculate)
+//        tvResult = findViewById(R.id.tv_result)
+//
+//        btnCalculate.setOnClickListener(this)
+
+        binding.btnCalculate.setOnClickListener(this)
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+            binding.tvResult.text = result
         }
     }
 
     override fun onClick(v: View) {
         if (v.id == R.id.btn_calculate) {
 
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
+            val inputLength = binding.edtLength.text.toString().trim()
+            val inputWidth = binding.edtWidth.text.toString().trim()
+            val inputHeight = binding.edtHeight.text.toString().trim()
 
             var isEmptyFields = false
 
             if (inputLength.isEmpty()) {
                 isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
+                binding.edtLength.error = "Field ini tidak boleh kosong"
             }
             if (inputWidth.isEmpty()) {
                 isEmptyFields = true
-                edtWidth.error = "Field ini tidak boleh kosong"
+                binding.edtWidth.error = "Field ini tidak boleh kosong"
             }
             if (inputHeight.isEmpty()) {
                 isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
+                binding.edtHeight.error = "Field ini tidak boleh kosong"
             }
 
             if (!isEmptyFields) {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+                binding.tvResult.text = volume.toString()
             }
         }
     }
