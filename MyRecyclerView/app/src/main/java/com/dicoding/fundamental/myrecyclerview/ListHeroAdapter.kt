0954a11e211a5380cea6030239a6883ef88f9da8
fachridantm <fachridantm@gmail.com>
2022-03-01 00:04:03 +0700

    ✨ Update ListHeroAdapter.kt using ViewBinding

--- MyRecyclerView/app/src/main/java/com/dicoding/fundamental/myrecyclerview/ListHeroAdapter.kt
+++ MyRecyclerView/app/src/main/java/com/dicoding/fundamental/myrecyclerview/ListHeroAdapter.kt
@@ -1,12 +1,10 @@
 package com.dicoding.fundamental.myrecyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.dicoding.fundamental.myrecyclerview.databinding.ItemRowHeroBinding
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -17,10 +15,12 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
         this.onItemClickCallback = onItemClickCallback
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+//        val view: View =
+//            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+//        return ListViewHolder(view)
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
@@ -28,9 +28,9 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
         Glide.with(holder.itemView.context)
             .load(photo) // URL Gambar
             .circleCrop() // Mengubah image menjadi lingkaran
-            .into(holder.imgPhoto) // imageView mana yang akan diterapkan
-        holder.tvName.text = name
-        holder.tvDescription.text = description
+            .into(holder.binding.imgItemPhoto) // imageView mana yang akan diterapkan
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
         holder.itemView.setOnClickListener {
             onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
         }
@@ -38,10 +38,10 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
 
     override fun getItemCount(): Int = listHero.size
 
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+//        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+//        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+//        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
 
     interface OnItemClickCallback {
