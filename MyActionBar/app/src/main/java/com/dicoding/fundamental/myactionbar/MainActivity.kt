b874646657a77f5b796a2283fc14608e78df6784
fachridantm <fachridantm@gmail.com>
2022-03-07 16:17:34 +0700

    ✨ Update Project MyActionBar

--- MyActionBar/app/src/main/java/com/dicoding/fundamental/myactionbar/MainActivity.kt
+++ MyActionBar/app/src/main/java/com/dicoding/fundamental/myactionbar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.dicoding.fundamental.myactionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.dicoding.fundamental.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+
+        searchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                /*
+                Gunakan method ini ketika search selesai atau OK
+                */
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                    searchView.clearFocus()
+                    return true
+                }
+
+                /*
+                Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+                */
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return false
+                }
+            })
         return true
     }
 
