7bcd158e2f7eba090feaee28fb8f7eb081f5bb53
fachridantm <fachridantm@gmail.com>
2022-03-11 22:16:44 +0700

    ✨ Build MyQuote project

---
+++ MyQuote/app/src/main/java/com/dicoding/fundamental/myquote/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.dicoding.fundamental.myquote
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.fundamental.myquote.activity.ListQuotesActivity
+import com.dicoding.fundamental.myquote.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        getRandomQuote()
+
+        mainBinding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
+    }
+
+    private fun getRandomQuote() {
+        mainBinding.progressBar.visibility = View.VISIBLE
+
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/random"
+
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                // Jika koneksi berhasil
+                mainBinding.progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+                    val quote = responseObject.getString("en")
+                    val author = responseObject.getString("author")
+                    mainBinding.tvQuote.text = quote
+                    mainBinding.tvAuthor.text = author
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                // Jika koneksi gagal
+                mainBinding.progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
