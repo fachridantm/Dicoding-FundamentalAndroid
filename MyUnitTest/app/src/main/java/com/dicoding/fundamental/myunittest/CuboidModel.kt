b91f0ac64a55507fe23eb01daab707bfb4642394
fachridantm <fachridantm@gmail.com>
2022-03-21 20:01:37 +0700

    ✨ Create CuboidModel.kt

---
+++ MyUnitTest/app/src/main/java/com/dicoding/fundamental/myunittest/CuboidModel.kt
@@ -0,0 +1,20 @@
+package com.dicoding.fundamental.myunittest
+
+class CuboidModel {
+    private var width = 0.0
+    private var length = 0.0
+    private var height = 0.0
+    fun getVolume(): Double = width * length * height
+    fun getSurfaceArea(): Double {
+        val wl = width * length
+        val wh = width * height
+        val lh = length * height
+        return 2 * (wl + wh + lh)
+    }
+    fun getCircumference(): Double = 4 * (width + length + height)
+    fun save(width: Double, length: Double, height: Double) {
+        this.width = width
+        this.length = length
+        this.height = height
+    }
+}
