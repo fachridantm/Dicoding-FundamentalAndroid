8aa146d5a625b9a350add92e23942985cac9d478
fachridantm <fachridantm@gmail.com>
2022-03-22 23:07:53 +0700

    ✨ Implement parcelable in UserModel.kt

--- MySharedPreferences/app/src/main/java/com/dicoding/fundamental/mysharedpreferences/UserModel.kt
+++ MySharedPreferences/app/src/main/java/com/dicoding/fundamental/mysharedpreferences/UserModel.kt
@@ -1,9 +1,13 @@
 package com.dicoding.fundamental.mysharedpreferences
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserModel (
     var name: String? = null,
     var email: String? = null,
     var age: Int = 0,
     var phoneNumber: String? = null,
     var isLove: Boolean = false
-)
+) : Parcelable
