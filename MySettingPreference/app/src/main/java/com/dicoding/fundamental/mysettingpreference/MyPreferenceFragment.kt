63dad57df1077d10e02b8397b63a13e3ae445732
fachridantm <fachridantm@gmail.com>
2022-03-23 00:02:56 +0700

    ✨ Update import MyPreferenceFragment.kt

--- MySettingPreference/app/src/main/java/com/dicoding/fundamental/mysettingpreference/MyPreferenceFragment.kt
+++ MySettingPreference/app/src/main/java/com/dicoding/fundamental/mysettingpreference/MyPreferenceFragment.kt
@@ -1,5 +1,8 @@
 package com.dicoding.fundamental.mysettingpreference
 
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
 class MyPreferenceFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(bundle: Bundle?, s: String?) {
