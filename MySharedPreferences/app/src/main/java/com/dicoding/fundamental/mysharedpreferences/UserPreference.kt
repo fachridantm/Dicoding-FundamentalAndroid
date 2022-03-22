3d6168340f5acec3b60e02f10b4d67c93cb0b4e5
fachridantm <fachridantm@gmail.com>
2022-03-22 23:49:39 +0700

    ✨ Add UserPreference.kt

---
+++ MySharedPreferences/app/src/main/java/com/dicoding/fundamental/mysharedpreferences/UserPreference.kt
@@ -0,0 +1,35 @@
+package com.dicoding.fundamental.mysharedpreferences
+
+import android.content.Context
+
+internal class UserPreference(context: Context) {
+    companion object {
+        private const val PREFS_NAME = "user_pref"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val AGE = "age"
+        private const val PHONE_NUMBER = "phone"
+        private const val LOVE_MU = "islove"
+    }
+
+    private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    fun setUser(value: UserModel) {
+        val editor = preferences.edit()
+        editor.putString(NAME, value.name)
+        editor.putString(EMAIL, value.email)
+        editor.putInt(AGE, value.age)
+        editor.putString(PHONE_NUMBER, value.phoneNumber)
+        editor.putBoolean(LOVE_MU, value.isLove)
+        editor.apply()
+    }
+
+    fun getUser(): UserModel {
+        val model = UserModel()
+        model.name = preferences.getString(NAME, "")
+        model.email = preferences.getString(EMAIL, "")
+        model.age = preferences.getInt(AGE, 0)
+        model.phoneNumber = preferences.getString(PHONE_NUMBER, "")
+        model.isLove = preferences.getBoolean(LOVE_MU, false)
+        return model
+    }
+}
