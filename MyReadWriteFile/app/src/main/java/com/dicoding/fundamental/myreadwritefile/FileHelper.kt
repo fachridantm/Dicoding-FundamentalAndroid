05c14c4e9e4c31b42536a2eabc6d5ba12d8b7210
fachridantm <fachridantm@gmail.com>
2022-03-22 09:07:42 +0700

    ✨ Create FileHelper.kt

---
+++ MyReadWriteFile/app/src/main/java/com/dicoding/fundamental/myreadwritefile/FileHelper.kt
@@ -0,0 +1,22 @@
+package com.dicoding.fundamental.myreadwritefile
+
+import android.content.Context
+
+internal object FileHelper {
+
+    fun writeToFile(fileModel: FileModel, context: Context) {
+        context.openFileOutput(fileModel.filename, Context.MODE_PRIVATE).use {
+            it.write(fileModel.data?.toByteArray())
+        }
+    }
+    fun readFromFile(context: Context, filename: String): FileModel {
+        val fileModel = FileModel()
+        fileModel.filename = filename
+        fileModel.data = context.openFileInput(filename).bufferedReader().useLines { lines ->
+            lines.fold("") { some, text ->
+                "$some\n$text"
+            }
+        }
+        return fileModel
+    }
+}
