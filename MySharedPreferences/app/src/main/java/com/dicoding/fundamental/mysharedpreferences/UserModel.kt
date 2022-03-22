9134c89c7d5ead5b2a43f0923e7518341a73f924
fachridantm <fachridantm@gmail.com>
2022-03-22 23:06:05 +0700

    ✨ Add UserModel.kt

---
+++ MySharedPreferences/app/src/main/java/com/dicoding/fundamental/mysharedpreferences/UserModel.kt
@@ -0,0 +1,9 @@
+package com.dicoding.fundamental.mysharedpreferences
+
+data class UserModel (
+    var name: String? = null,
+    var email: String? = null,
+    var age: Int = 0,
+    var phoneNumber: String? = null,
+    var isLove: Boolean = false
+)
