688c9de190e4559b390fdf2317813e3a9a478202
fachridantm <fachridantm@gmail.com>
2022-03-23 00:04:13 +0700

    ✨ Update MainActivity.kt

--- MySettingPreference/app/src/main/java/com/dicoding/fundamental/mysettingpreference/MainActivity.kt
+++ MySettingPreference/app/src/main/java/com/dicoding/fundamental/mysettingpreference/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.dicoding.fundamental.mysettingpreference
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.setting_holder, MyPreferenceFragment())
+            .commit()
+
     }
 }
