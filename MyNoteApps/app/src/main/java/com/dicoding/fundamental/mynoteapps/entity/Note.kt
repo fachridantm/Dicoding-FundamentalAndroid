45838ea9f8ab95559eff434060a4c3681258f4f6
fachridantm <fachridantm@gmail.com>
2022-03-23 16:56:23 +0700

    ✨ Create data class Note.kt

---
+++ MyNoteApps/app/src/main/java/com/dicoding/fundamental/mynoteapps/entity/Note.kt
@@ -0,0 +1,12 @@
+package com.dicoding.fundamental.mynoteapps.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Note(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var date: String? = null
+) : Parcelable
