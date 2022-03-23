eff781010bac85773f7f91896f722bbb0b8a8f12
fachridantm <fachridantm@gmail.com>
2022-03-23 15:36:03 +0700

    ✨ Update MainActivity.kt

--- MyDataStore/app/src/main/java/com/dicoding/fundamental/mydatastore/MainActivity.kt
+++ MyDataStore/app/src/main/java/com/dicoding/fundamental/mydatastore/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.dicoding.fundamental.mydatastore
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.fundamental.mydatastore.databinding.ActivityMainBinding
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,15 +22,23 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        val pref = SettingPreferences.getInstance(dataStore)
+        val mainViewModel =
+            ViewModelProvider(this, ViewModelFactory(pref))[MainViewModel::class.java]
+
         mainBinding.apply {
-            switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
-                if (isChecked) {
+            mainViewModel.getThemeSettings().observe(this@MainActivity) { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                     switchTheme.isChecked = true
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                     switchTheme.isChecked = false
                 }
+
+            }
+            switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+                mainViewModel.saveThemeSetting(isChecked)
             }
         }
     }
