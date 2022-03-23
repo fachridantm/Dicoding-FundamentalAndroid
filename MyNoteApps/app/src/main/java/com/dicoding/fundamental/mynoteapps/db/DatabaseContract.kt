09a21d03895a6b9e52e2b8f65050d1fb779a2c3f
fachridantm <fachridantm@gmail.com>
2022-03-23 21:31:35 +0700

    ✨ Create DatabaseContract.kt

---
+++ MyNoteApps/app/src/main/java/com/dicoding/fundamental/mynoteapps/db/DatabaseContract.kt
@@ -0,0 +1,15 @@
+package com.dicoding.fundamental.mynoteapps.db
+
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class NoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_NAME = "note"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+        }
+    }
+}
