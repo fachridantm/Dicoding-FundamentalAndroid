495159f5dac976e91ee0c11dae2d0dbc8e52bcb2
fachridantm <fachridantm@gmail.com>
2022-03-23 08:11:26 +0700

    ✨ Update MainActivity.kt using viewBinding

--- MyDataStore/app/src/main/java/com/dicoding/fundamental/mydatastore/MainActivity.kt
+++ MyDataStore/app/src/main/java/com/dicoding/fundamental/mydatastore/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.dicoding.fundamental.mydatastore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.dicoding.fundamental.mydatastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.apply {
+            switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    switchTheme.isChecked = true
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    switchTheme.isChecked = false
+                }
+            }
+        }
     }
 }
