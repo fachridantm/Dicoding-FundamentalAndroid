9ed3cf05b2b1a45422d8ede97d167d9fe1f4197d
fachridantm <fachridantm@gmail.com>
2022-07-13 16:08:30 +0700

    💻 Latihan Broadcast Receiver dari Custom Event

---
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/fundamental/mybroadcastreceiver/broadcast/DownloadService.kt
@@ -0,0 +1,31 @@
+package com.dicoding.fundamental.mybroadcastreceiver.broadcast
+
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import com.dicoding.fundamental.mybroadcastreceiver.ui.MainActivity
+
+class DownloadService : JobIntentService() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            enqueueWork(this, this::class.java, 101, intent)
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onHandleWork(intent: Intent) {
+        Log.d(TAG, "Download Service dijalankan")
+        try {
+            Thread.sleep(5000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        val notifyFinishIntent = Intent(MainActivity.ACTION_DOWNLOAD_STATUS)
+        sendBroadcast(notifyFinishIntent)
+    }
+
+    companion object {
+        val TAG: String = DownloadService::class.java.simpleName
+    }
+}
--- MyBroadcastReceiver/app/src/main/java/com/dicoding/fundamental/mybroadcastreceiver/ui/MainActivity.kt
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/fundamental/mybroadcastreceiver/ui/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.dicoding.fundamental.mybroadcastreceiver.ui
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.fundamental.mybroadcastreceiver.R
+import com.dicoding.fundamental.mybroadcastreceiver.broadcast.DownloadService
 import com.dicoding.fundamental.mybroadcastreceiver.broadcast.PermissionManager
 import com.dicoding.fundamental.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +28,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View) {
@@ -30,25 +47,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 Manifest.permission.RECEIVE_SMS,
                 SMS_REQUEST_CODE
             )
+            R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == SMS_REQUEST_CODE) {
             when (PackageManager.PERMISSION_GRANTED) {
-                grantResults[0] -> Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
-                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+                grantResults[0] -> Toast.makeText(
+                    this,
+                    "Sms receiver permission diterima",
+                    Toast.LENGTH_SHORT
+                ).show()
+                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 
     companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
         private const val SMS_REQUEST_CODE = 101
     }
 }
